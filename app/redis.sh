b1ee29bd278ed575387b9208457d91c7dd5c3c6f
Indra Santosa <indrasantosa90@gmail.com>
2014-10-07 10:07:56 +0800

    Add redis installation script

--- app/redis.sh
+++ app/redis.sh
@@ -3,4 +3,10 @@ sudo apt-get install build-essential
 sudo apt-get install tcl8.5
 
 wget http://download.redis.io/releases/redis-2.8.9.tar.gz
-#tar xzf redis-2.8.9.tar.gz
+tar xzf redis-2.8.9.tar.gz
+cd redis-2.8.9
+make
+make test
+sudo make install
+cd utils
+sudo ./install_server.sh
